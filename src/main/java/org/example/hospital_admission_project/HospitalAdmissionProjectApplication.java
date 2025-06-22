ded5804e580cd8e82eafded39698d0bc9729ed80
DiyanQadamboyev <diyorbekqadamboyev.0208@gmail.com>
2025-06-22 19:19:42 +0500

    commit

---
+++ src/main/java/org/example/hospital_admission_project/HospitalAdmissionProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.hospital_admission_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalAdmissionProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalAdmissionProjectApplication.class, args);
+    }
+
+}
